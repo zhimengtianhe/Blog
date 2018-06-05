4c1383f53d097dff46c18487aa620357c51144d9
zhangxiao <xiao.zhang@nanguadl.com>
2018-06-05 16:20:57 +0800

    1

---
+++ src/components/common/Left.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="Left">
+		<div class="Nav">
+			<div class="NavText">
+				<router-link  v-for="(nav,index) in navList" :key="index" :to="nav.path">{{nav.text}}</router-link>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'Left',
+	  	data () {
+	    	return {
+				navList: [
+					{
+						path: 'Personal',
+						text: '自我介绍'
+					},
+					{
+						path: 'TimeAxis',
+						text: '时光轴'
+					},
+					{
+						path: 'Index',
+						text: '博客'
+					},
+				]
+	    	}
+		},
+		mounted(){
+       
+		},
+		created(){
+
+		},
+		methods:{
+			
+		}
+		
+	}
+</script>
+<style lang="scss" scoped>
+	.Left{
+		max-width: 465px;
+    	width: 26%;
+		height: 100%;
+		background: url(../../assets/IndexLeft.jpg) top  no-repeat;
+		background-size:cover;
+		float: left;
+		position: fixed;
+		color: #fff;
+		font-family: Georgia,"Times New Roman",Times,Kai,"Kaiti SC",KaiTi,BiauKai,FontAwesome,serif;
+		.NavText{
+			float: right;
+			a{
+				width: 39px;
+				float: left;
+				display: block;
+				font-size: 20px;
+				font-weight: 800;
+				color: #fff;
+			}
+			a:hover{
+				color: purple;
+			}
+		}
+				
+	}
+</style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,40 +1,21 @@
 <template>
 	<div class="Index">
 		<div class="content">
-			<div class="Left">
-				<div class="Nav">
-					<div class="NavText">
-						<router-link v-for="(nav,index) in navList" :key="index" :to="nav.path">{{nav.text}}</router-link>
-					</div>
-				</div>
-			</div>
-			<div class="Right">
-				
-			</div>
+			<Left></Left>
 		</div>
 	</div>
 </template>
 
 <script>
+	import Left from "./common/Left.vue";
 	export default{
 		name: 'Index',
+		components:{
+    		Left
+ 		},
 	  	data () {
 	    	return {
-	      		msg: '首页',
-				navList: [
-					{
-						path: 'Personal',
-						text: '自我介绍'
-					},
-					{
-						path: 'TimeAxis',
-						text: '时光轴'
-					},
-					{
-						path: 'Index',
-						text: '博客'
-					},
-				]
+				
 	    	}
 		},
 		mounted(){
@@ -46,6 +27,7 @@
 		methods:{
 			
 		}
+		
 	}
 </script>
 
@@ -56,31 +38,6 @@
 		.content{
 			height: 100%;
 			overflow: hidden;
-			.Left{
-				max-width: 465px;
-    			width: 26%;
-				height: 100%;
-				background: url(../assets/IndexLeft.jpg) top  no-repeat;
-				background-size:cover;
-				float: left;
-				position: fixed;
-				color: #fff;
-				.NavText{
-					float: right;
-					a{
-						width: 39px;
-						float: left;
-						display: block;
-						font-size: 20px;
-						font-weight: 800;
-						color: #fff;
-					}
-					a:hover{
-						color: purple;
-					}
-				}
-				
-			}
 			.Right{
 				width: 74%;
 				height: 2000px;
